4abf3219a2fc3d33e658d03dc0a9a43d43c07941
canox82 <canox82@gmail.com>
2024-10-11 12:56:01 +0200

    Base pagina Progetti

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -12,17 +12,20 @@ export const languages = {
         'nav.blog': 'Novità',
         'nav.about': 'Su di noi',
         'nav.coworking': 'Coworking',
+        'nav.projects': 'Progetti',
       },
     en: {
       'nav.home': 'Home',
       'nav.blog': 'News',
       'nav.about': 'About Us',
       'nav.coworking': 'Coworking',
+      'nav.projects': 'Projects',
     },
     de: {
         'nav.home': 'Home',
         'nav.blog': 'News',
         'nav.about': 'Uber uns',
         'nav.coworking': 'Coworking',
+        'nav.projects': 'Projekte',
       },
   } as const;
