60eb63efb5d379ade73f05933c13df8c82de00f5
canox82 <canox82@gmail.com>
2024-10-11 10:00:01 +0200

    cowork

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,15 +11,18 @@ export const languages = {
         'nav.home': 'Home',
         'nav.blog': 'Novità',
         'nav.about': 'Su di noi',
+        'nav.coworking': 'Coworking',
       },
     en: {
       'nav.home': 'Home',
       'nav.blog': 'News',
       'nav.about': 'About Us',
+      'nav.coworking': 'Coworking',
     },
     de: {
         'nav.home': 'Home',
         'nav.blog': 'News',
         'nav.about': 'Uber uns',
+        'nav.coworking': 'Coworking',
       },
   } as const;
